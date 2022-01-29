5475bc9e0f5907bafc0bb8c155a9d9b01b3aafa3
BrianQuintero <63617101+BrianQuintero@users.noreply.github.com>
2022-01-29 15:48:41 -0800

    Added customer burger creator

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Customer : MonoBehaviour
 {
+    List<string> ingredients = new List<string>();
+    List<string> burger = new List<string>();
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        ingredients.Add("bun");
+        ingredients.Add("lettuce");
+        ingredients.Add("tomato");
+        ingredients.Add("cheese");
+        ingredients.Add("patty");
+    }
+
+    void generateBurger(int size)
+    {
+        burger.Add(ingredients[0]);
+        for(int i = 0; i < size; i++)
+        {
+            burger.Add(ingredients[UnityEngine.Random.Range(1, 5)]);
+        }
+        burger.Add(ingredients[0]);
+        for(int j = 0; j < burger.Count; j++)
+        {
+            UnityEngine.Debug.Log(burger[j]);
+        }
+        //UnityEngine.Debug.Log(burger.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            generateBurger(3);
+        }
     }
 }
