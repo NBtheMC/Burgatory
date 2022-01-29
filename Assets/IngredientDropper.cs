f419f332c216edcda35f3b5c27ce7728dffcdb66
Naman Bhushan <49595001+NBtheMC@users.noreply.github.com>
2022-01-28 22:12:51 -0800

    Spawn timers started

--- Assets/IngredientDropper.cs
+++ Assets/IngredientDropper.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IngredientDropper : MonoBehaviour
 {
@@ -10,13 +11,35 @@ public class IngredientDropper : MonoBehaviour
     private float timer;
     private float range;
 
+    //PREFABS
     [SerializeField] private GameObject lettuce;
     [SerializeField] private GameObject tomato;
-    [SerializeField] private GameObject bun;
     [SerializeField] private GameObject cheese;
+    [SerializeField] private GameObject bun;
+
+    //SPAWNING BUTTONS
+    [SerializeField] private Button lettuceButton;
+    [SerializeField] private Button tomatoButton;
+    [SerializeField] private Button cheeseButton;
+    [SerializeField] private Button bunButton;
+
+    //TIMER LOGIC
+    private bool lettuceCanSpawn;
+    private bool tomatoCanSpawn;
+    private bool cheeseCanSpawn;
+    private bool bunCanSpawn;
+
+    private float lettuceTimer;
+    private float tomatoTimer;
+    private float cheeseTimer;
+    private float bunTimer;
 
     void Awake(){
         //ingredientsToDrop = new List<string>() 
+        lettuceCanSpawn = true;
+        tomatoCanSpawn = true;
+        cheeseCanSpawn = true;
+        bunCanSpawn = true;
     }
 
     // Start is called before the first frame update
@@ -37,15 +60,21 @@ public class IngredientDropper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if(timer >= speed){
-            if(ingredientsToDrop.Count == 0){
-                EndDrop();
-            }
-            DropIngredient();
-            ingredientsToDrop.RemoveAt(0);
-            timer = 0;
-        }
+        // One at a time dropping based on list
+        // timer += Time.deltaTime;
+        // if(timer >= speed){
+        //     if(ingredientsToDrop.Count == 0){
+        //         EndDrop();
+        //     }
+        //     DropIngredient();
+        //     ingredientsToDrop.RemoveAt(0);
+        //     timer = 0;
+        // }
+        // Timer updating
+        lettuceTimer -= Time.deltaTime;
+        tomatoTimer -= Time.deltaTime;
+        cheeseTimer -= Time.deltaTime;
+        bunTimer -= Time.deltaTime;
     }
 
     //Used by some other script to set up order of ingredients to drop
@@ -56,21 +85,33 @@ public class IngredientDropper : MonoBehaviour
 
 
     //Get next ingredient in drop list and drop it
-    void DropIngredient(){
+    public void DropIngredient(string ingredient){
         Vector3 positionToDrop = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z) + new Vector3(Random.Range(-range, range), 0, 0);
-        string nextIngredientToDrop = ingredientsToDrop[0];
-        switch(nextIngredientToDrop){
+        //string nextIngredientToDrop = ingredientsToDrop[0];
+        switch(ingredient){
             case "lettuce":
-                Instantiate(lettuce, positionToDrop, new Quaternion(0f, 0f, 0f, 0f));
+                if(lettuceTimer <= 0){
+                    Instantiate(lettuce, positionToDrop, new Quaternion(0f, 0f, 0f, 0f));
+                    lettuceTimer = timer;
+                }
                 break;
             case "tomato":
-                Instantiate(tomato, positionToDrop, new Quaternion(0f, 0f, 0f, 0f));
-                break;
-            case "bun":
-                Instantiate(bun, positionToDrop, new Quaternion(0f, 0f, 0f, 0f));
+                if(tomatoTimer <= 0){
+                    Instantiate(tomato, positionToDrop, new Quaternion(0f, 0f, 0f, 0f));
+                    tomatoTimer = timer;
+                }                
                 break;
             case "cheese":
-                Instantiate(cheese, positionToDrop, new Quaternion(0f, 0f, 0f, 0f));
+                if(cheeseTimer <= 0){
+                    Instantiate(cheese, positionToDrop, new Quaternion(0f, 0f, 0f, 0f));
+                    cheeseTimer = timer;
+                }
+                break;
+            case "bun":
+                if(bunTimer <= 0){
+                    Instantiate(bun, positionToDrop, new Quaternion(0f, 0f, 0f, 0f));
+                    bunTimer = timer;
+                }
                 break;
         }
     }
