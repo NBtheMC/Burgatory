bed40fd473636a06d2487063fe7b75b5fe791a9d
Naman Bhushan <49595001+NBtheMC@users.noreply.github.com>
2022-01-28 22:15:26 -0800

    uhhhhh

---
+++ Assets/Scripts/BalanceChecker.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BalanceChecker : MonoBehaviour
+{
+
+    public GameObject arrow;
+    private float balanceNum;
+    private float currentRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        balanceNum = 0f;
+        currentRotation = 0f;
+        arrow.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Debug.Log(currentRotation);
+        if (Input.GetKeyDown("q"))
+        {
+            increaseNumber(10);
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            increaseNumber(-10);
+        }
+    }
+
+    void increaseNumber(int num)
+    {
+        //currentRotation += num;
+        balanceNum += num;
+        rotateArrow();
+    }
+
+    void rotateArrow()
+    {
+        arrow.transform.rotation = Quaternion.Euler(0.0f, 0.0f, balanceNum);
+    }
+}
