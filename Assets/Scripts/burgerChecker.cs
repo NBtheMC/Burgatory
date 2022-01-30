4c4ab4712f0a1a2495935a7bac40b3ee29806990
BrianQuintero <63617101+BrianQuintero@users.noreply.github.com>
2022-01-29 20:24:58 -0800

    updated burger checker (it seems to work!)

--- Assets/Scripts/burgerChecker.cs
+++ Assets/Scripts/burgerChecker.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Globalization;
 using UnityEngine;
 
 public class burgerChecker : MonoBehaviour
 {
     private GameObject[] ingredientsInField;
+    private List<string> burgerMade = new List<string>();
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +16,35 @@ public class burgerChecker : MonoBehaviour
 
     public void checkBurger()
     {
+        
         ingredientsInField = GameObject.FindGameObjectsWithTag("Ingredient");
         if (ingredientsInField.Length > 0)
         {
             for (int i = 0; i < ingredientsInField.Length; i++)
             {
-                UnityEngine.Debug.Log(ingredientsInField[i].name);
+                //UnityEngine.Debug.Log(ingredientsInField[i].name);
+                if (ingredientsInField[i].name.IndexOf("lettuce", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    burgerMade.Add("lettuce");
+                }
+                else if (ingredientsInField[i].name.IndexOf("bun", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    burgerMade.Add("bun");
+                }
+                else if (ingredientsInField[i].name.IndexOf("cheese", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    burgerMade.Add("cheese");
+                }
+                else if (ingredientsInField[i].name.IndexOf("patty", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    burgerMade.Add("patty");
+                }
+                else if (ingredientsInField[i].name.IndexOf("tomato", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    burgerMade.Add("tomato");
+                }
             }
+            
         }
     }
 
